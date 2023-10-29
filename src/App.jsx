a4a6531df3b98cb313a2afa0c8fab2cb9b0b7a7b
Sergey UP <sergey10goals@gmail.com>
2023-10-29 16:28:28 +0200

    decompose to parent and child>> mponents

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import Button from './components/Button/Button';
+import CardButton from './components/CardButton/CardButton';
 import JournalItem from './components/JournalItem/JournalItem';
 
 function App() {
@@ -21,16 +22,20 @@ function App() {
       <h1>Start Project</h1>
       <p>some text here</p>
       <Button />
-      <JournalItem
-        title={data[0].title}
-        text={data[0].text}
-        date={data[0].date}
-      />
-      <JournalItem
-        title={data[1].title}
-        text={data[1].text}
-        date={data[1].date}
-      />
+      <CardButton>
+        <JournalItem
+          title={data[0].title}
+          text={data[0].text}
+          date={data[0].date}
+        />
+      </CardButton>
+      <CardButton>
+        <JournalItem
+          title={data[1].title}
+          text={data[1].text}
+          date={data[1].date}
+        />
+      </CardButton>
     </>
   );
 }
--- src/components/JournalItem/JournalItem.jsx
+++ src/components/JournalItem/JournalItem.jsx
@@ -4,13 +4,13 @@ function JournalItem({ title, text, date }) {
   const formatedDate = new Intl.DateTimeFormat('ua-UA').format(date);
 
   return (
-    <div className="journal-item">
+    <>
       <h2 className="journal-item__title">{title}</h2>
       <div className="journal-item__content">
         <p className="journal-item__date">{formatedDate}</p>
         <p className="journal-item__text">{text}</p>
       </div>
-    </div>
+    </>
   );
 }
 
