e0e30981be6ab4e80876bd44b8ad71c14a4e82f0
Sergey UP <sergey10goals@gmail.com>
2023-10-30 12:42:04 +0200

    update Button & CardButton components

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,8 @@
 import './Button.css';
 
-function Button() {
-
+function Button({ text, onClick }) {
   return (
-    <button className='button accent'>Save</button>
+    <button className='button accent' onClick={onClick}>{text}</button>
   );
 }
 
