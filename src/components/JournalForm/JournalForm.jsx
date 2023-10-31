ea6098b58f3b16fb6c1b3afb30ff6397904c669f
Sergey UP <sergey10goals@gmail.com>
2023-10-31 17:38:16 +0200

    add useRef to form validation

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -1,17 +1,34 @@
 import './JournalForm.css';
 import Button from '../Button/Button';
-import { useEffect, useReducer } from 'react';
+import { useEffect, useReducer, useRef } from 'react';
 import classNames from 'classnames';
 import { INITIAL_STATE, formReducer } from './JournalForm.state';
 
 function JournalForm({ onSubmit }) {
 	const [formState, dispatchForm] = useReducer(formReducer, INITIAL_STATE);
 	const { isValid, isFormReadyToSubmit, values } = formState;
+	const titleRef = useRef();
+	const dateRef = useRef();
+	const textRef = useRef();
 
+	const focusError = (isValid) => {
+		switch (true) {
+			case !isValid.title:
+				titleRef.current.focus();
+				break;
+			case !isValid.date:
+				dateRef.current.focus();
+				break;
+			case !isValid.text:
+				textRef.current.focus();
+				break;
+		}
+	};
 
 	useEffect(() => {
 		let timerId;
 		if (!isValid.title || !isValid.data || !isValid.text) {
+			focusError(isValid);
 			timerId = setTimeout(() => {
 				dispatchForm({ type: 'RESET_VALIDITY' });
 			}, 2000);
@@ -39,12 +56,12 @@ function JournalForm({ onSubmit }) {
 
 	return (
 		<form className='journal-form' onSubmit={addJournalItem}>
-			<input type='text' onChange={onChange} value={values.title} name='title' className={classNames('input', {
+			<input type='text' ref={titleRef} onChange={onChange} value={values.title} name='title' className={classNames('input', {
 				'invalid': !isValid.title
 			})} />
-			<input type='date' onChange={onChange} value={values.date} name='date' className={`input ${isValid.date ? '' : 'invalid'}`} />
+			<input type='date' ref={dateRef} onChange={onChange} value={values.date} name='date' className={`input ${isValid.date ? '' : 'invalid'}`} />
 			<input type='text' name='tag' />
-			<textarea name="text" onChange={onChange} value={values.text} cols="30" rows="10" className={`input ${isValid.text ? '' : 'invalid'}`} ></textarea>
+			<textarea name="text" ref={textRef} onChange={onChange} value={values.text} cols="30" rows="10" className={`input ${isValid.text ? '' : 'invalid'}`} ></textarea>
 			<Button text="Сохранить" />
 		</form>
 	);
