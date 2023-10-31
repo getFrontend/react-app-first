2a931ba736832fbdffee19a8fffe6521b23b9f24
Sergey UP <sergey10goals@gmail.com>
2023-10-31 17:08:39 +0200

    add form clearing

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -4,26 +4,14 @@ import { useEffect, useReducer } from 'react';
 import classNames from 'classnames';
 import { INITIAL_STATE, formReducer } from './JournalForm.state';
 
-// const INITIAL_STATE = {
-// 	title: true,
-// 	text: true,
-// 	date: true
-// };
-
 function JournalForm({ onSubmit }) {
-	// const [formValidState, setFormValidState] = useState(
-	// 	INITIAL_STATE
-	// );
 	const [formState, dispatchForm] = useReducer(formReducer, INITIAL_STATE);
 	const { isValid, isFormReadyToSubmit, values } = formState;
 
 
 	useEffect(() => {
 		let timerId;
-		if (!isValid.title ||
-			!isValid.data ||
-			!isValid.text
-		) {
+		if (!isValid.title || !isValid.data || !isValid.text) {
 			timerId = setTimeout(() => {
 				dispatchForm({ type: 'RESET_VALIDITY' });
 			}, 2000);
@@ -36,24 +24,27 @@ function JournalForm({ onSubmit }) {
 	useEffect(() => {
 		if (isFormReadyToSubmit) {
 			onSubmit(values);
+			dispatchForm({ type: 'CLEAR' });
 		}
 	}, [isFormReadyToSubmit]);
 
+	const onChange = (e) => {
+		dispatchForm({ type: 'SET_VALUE', payload: { [e.target.name]: e.target.value } });
+	};
+
 	const addJournalItem = (e) => {
 		e.preventDefault();
-		const formData = new FormData(e.target);
-		const formProps = Object.fromEntries(formData);
-		dispatchForm({ type: 'SUBMIT', payload: formProps });
+		dispatchForm({ type: 'SUBMIT' });
 	};
 
 	return (
 		<form className='journal-form' onSubmit={addJournalItem}>
-			<input type='text' name='title' className={classNames('input', {
+			<input type='text' onChange={onChange} value={values.title} name='title' className={classNames('input', {
 				'invalid': !isValid.title
 			})} />
-			<input type='date' name='date' className={`input ${isValid.date ? '' : 'invalid'}`} />
+			<input type='date' onChange={onChange} value={values.date} name='date' className={`input ${isValid.date ? '' : 'invalid'}`} />
 			<input type='text' name='tag' />
-			<textarea name="text" id="" cols="30" rows="10" className={`input ${isValid.text ? '' : 'invalid'}`} ></textarea>
+			<textarea name="text" onChange={onChange} value={values.text} cols="30" rows="10" className={`input ${isValid.text ? '' : 'invalid'}`} ></textarea>
 			<Button text="Сохранить" />
 		</form>
 	);
--- src/components/JournalForm/JournalForm.state.js
+++ src/components/JournalForm/JournalForm.state.js
@@ -5,23 +5,29 @@ export const INITIAL_STATE = {
     text: true
   },
   values: {
-    title: undefined,
-    date: undefined,
-    text: undefined
+    title: '',
+    date: '',
+    text: ''
   },
   isFormReadyToSubmit: false
 };
 
 export const formReducer = (state, action) => {
   switch (action.type) {
+    case 'SET_VALUE':
+      return {
+        ...state, values: { ...state.values, ...action.payload }
+      };
+    case 'CLEAR':
+      return { ...state, values: INITIAL_STATE.values };
     case 'RESET_VALIDITY':
       return { ...state, isValid: INITIAL_STATE.isValid };
     case 'SUBMIT': {
-      const titleValidity = action.payload.title?.trim().length;
-      const dateValidity = action.payload.date?.trim().length;
-      const textValidity = action.payload.text?.trim().length;
+      const titleValidity = state.values.title?.trim().length;
+      const dateValidity = state.values.date?.trim().length;
+      const textValidity = state.values.text?.trim().length;
       return {
-        values: action.payload,
+        ...state,
         isValid: {
           title: titleValidity,
           date: dateValidity,
