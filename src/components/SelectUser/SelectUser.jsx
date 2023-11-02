feec49cc29bd4a81332b194723ab04ba010110d4
Sergey UP <sergey10goals@gmail.com>
2023-11-02 16:16:13 +0200

    add SelectUser component

---
+++ src/components/SelectUser/SelectUser.jsx
@@ -0,0 +1,19 @@
+import { useContext } from 'react';
+import { UserContext } from '../../context/user.context';
+
+function SelectUser() {
+  const { userId, setUserId } = useContext(UserContext);
+
+  const changeUser = (e) => {
+    setUserId(e.target.value);
+  };
+
+  return (
+    <select name="user" id="user" value={userId} onChange={changeUser}>
+      <option value="1">User 1</option>
+      <option value="2">User 2</option>
+    </select>
+  );
+}
+
+export default SelectUser;
